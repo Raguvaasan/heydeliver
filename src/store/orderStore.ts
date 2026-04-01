f296c4a373f7573278d9e364975421f47e475c93
swetha <swey.shwetha@gmail.com>
2026-04-01 14:19:26 +0530

    changes

--- src/store/orderStore.ts
+++ src/store/orderStore.ts
@@ -567,6 +567,8 @@ export const useOrderStore = create<OrderState>((set, get) => ({
         "/api/hub/orders/create",
         {
           ...payload,
+          format: "json",
+          data: JSON.stringify(cmuPayload),
           ...(waybill ? { waybill } : {}),
         },
         {
